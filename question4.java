e32d7393b01ff4e9b5498ef3ea77a14d72e232fe
Abhimanyu <Abhimanyusaini13@gmail.com>
2018-09-14 03:14:40 +0530

    ds assignment

---
+++ question4.java
@@ -0,0 +1,17 @@
+class question4
+{
+   public static void main(String[] x)
+     {
+         String input = "abc";
+	 String s;
+         for(int i=0;i<3;i++)
+          {
+            for(int j=i+1;j<=3;j++)
+               {
+                 s = input.substring(i,j);
+		 System.out.println(s);
+                }		
+           }
+      }
+
+}
